004cc8580ca04acfbeea0ee1ba34643ee066c44b
Vincent Scaviner <vincent.scav@gmail.com>
2025-11-22 13:17:20 +0000

    Mise à jour du message d'alerte dans le cas ou le mode cron est déjà activé

--- lib/AppInfo/StartupCheck.php
+++ lib/AppInfo/StartupCheck.php
@@ -21,16 +21,28 @@ class StartupCheck
 
     public function checkBackgroundJob(): void
     {
-        $mode = \OC::$server->getSystemConfig()->getValue('backgroundjobs_mode', 'ajax');
-
-        if ($mode !== 'cron') {
-            $this->logger->warning("EmailBridge: Background job mode is not set to 'cron'. Please configure it in Nextcloud.");
-        }
-
+        $config = \OC::$server->getSystemConfig();
         $jobList = \OC::$server->getJobList();
+
+        // Vérifie si EmailSenderJob est enregistré
         if (!$jobList->has(EmailSenderJob::class, [])) {
             $this->logger->info("EmailBridge: EmailSenderJob not registered. Registering automatically.");
             $jobList->add(EmailSenderJob::class, []);
+        }
+
+        // Vérifie si le mode est cron et si le cron a été exécuté récemment
+        $mode = $config->getValue('backgroundjobs_mode', 'ajax');
+ 
+        if ($mode === 'cron') {
+            // Optionnel : on peut vérifier la dernière exécution pour être sûr
+            $lastCron = $config->getSystemValue('cron_last_run', 0); // timestamp
+            $diffMinutes = (time() - $lastCron) / 60;
+
+            if ($diffMinutes > 30) { // pas de cron depuis >30min
+                $this->logger->warning("EmailBridge: Background job mode is 'cron' but cron has not run in the last 30 minutes.");
+            }
+        } else {
+            $this->logger->warning("EmailBridge: Background job mode is not set to 'cron'. Please configure it in Nextcloud.");
         }
     }
 }
