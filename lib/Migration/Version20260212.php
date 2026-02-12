f44edc3c87d98bcbf171e9f312727920a4ac3863
Vincent Scaviner <vincent.scav@gmail.com>
2026-02-12 18:25:10 +0000

    add new migration and version 1.0.1 to prevent bug

---
+++ lib/Migration/Version20260212.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\EmailBridge\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+
+class Version20260212 extends Version20251020
+{
+}
+
